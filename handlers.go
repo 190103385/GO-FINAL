d762349784994e267b3a927e0c2b87a0e3cb897b
Nurdaulet Kurmangazyuly <190103385@stu.sdu.edu.kz>
2022-05-22 15:43:39 +0600

    Home page handler functionality added

--- handlers.go
+++ handlers.go
@@ -177,5 +177,37 @@ func Refresh(w http.ResponseWriter, r *http.Request) {
 
 //Home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Home"))
+	cookie, err := r.Cookie("token")
+	
+	if(err != nil) {
+		if(err == http.ErrNoCookie) {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	tokenStr := cookie.Value
+	claims := &Claims{}
+
+	tkn, err := jwt.ParseWithClaims(tokenStr, claims, func(t *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	if(err != nil) {
+		if(err == jwt.ErrSignatureInvalid) {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	w.Write([]byte(fmt.Sprintf("Hello %s", claims.Username)))
 }
