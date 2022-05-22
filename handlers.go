352b490b0e483bade7d95ac9ce27b415c7ac3305
Nurdaulet Kurmangazyuly <190103385@stu.sdu.edu.kz>
2022-05-22 12:28:43 +0600

    Added fields to work with JWT

--- handlers.go
+++ handlers.go
@@ -2,19 +2,40 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/dgrijalva/jwt-go"
 )
+
+//Key for JWT token
+var jwtKey = []byte("secret_key")
+
+//User credential's type
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+//Claims
+type Claims struct {
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
 
 func Login(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Login"))
 }
 
+
 func Register(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Register"))
 }
 
+
 func Refresh(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Refresh"))
 }
+
 
 func Home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Home"))
